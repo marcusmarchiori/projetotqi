94fa2c4d9bd0a0010bcc8d4e2c592f4d643a2c3c
Marcus <marcus_marchiori@hotmail.com>
2020-08-28 11:00:06 -0300

    primeirocommit

---
+++ src/main/java/br/com/projetotqi/projetotqi/ProjetotqiApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetotqi.projetotqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotqiApplication.class, args);
+	}
+
+}
